1aaf12e815f2a96df4ad0bf186b4daecfedb4ce6
saidurcse <saidursd@gmail.com>
2017-08-02 10:35:04 +0600

    Codeforeces problem solution

---
+++ 4A - Watermelon.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Simple {
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		int input = in.nextInt();
+		
+		if(input == 2)
+			System.out.println("NO");
+		else{				
+			if(input%2 ==0)
+					System.out.println("YES");			
+			else
+				System.out.println("NO");
+		}		
+	}
+}
---
+++ 71A - Way Too Long Words.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Simple{
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int input = in.nextInt();
+				
+		for(int i=0;i<=input;i++){
+			
+			String word = in.nextLine();
+			int len = word.length();
+			
+			if(len>10){
+				System.out.print(word.charAt(0));
+				System.out.print(len-2);
+				System.out.println(word.charAt(len-1));
+			}else{
+				System.out.println(word);
+			}
+		}
+	}
+}
